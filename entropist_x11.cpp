626b1cd439d875cae1e7a2f59cc7d1c280496a20
Oliver Lau <oliver@ersatzworld.net>
2018-06-18 07:27:19 +0200

    Update.

--- entropist_x11.cpp
+++ entropist_x11.cpp
@@ -10,19 +10,30 @@ void Entropist::runner(void)
   Display *display;
   Window root_window;
   XEvent event;
-
   display = XOpenDisplay(0);
   root_window = XRootWindow(display, 0);
   XSelectInput(display, root_window, PointerMotionMask);
-
-  while(true)
+  while (true)
   {
     XNextEvent(display, &event);
     switch(event.type)
     {
       case MotionNotify:
       {
-        printf("x %d y %d\n", event.xmotion.x, event.xmotion.y );
+        instance().hash.Update(reinterpret_cast<CryptoPP::byte *>(&event.xmotion.time), sizeof(event.xmotion.time) / sizeof(CryptoPP::byte));
+        instance().hash.Update(reinterpret_cast<CryptoPP::byte *>(&event.xmotion.x), sizeof(event.xmotion.x) / sizeof(CryptoPP::byte));
+        instance().hash.Update(reinterpret_cast<CryptoPP::byte *>(&event.xmotion.y), sizeof(event.xmotion.y) / sizeof(CryptoPP::byte));
+        instance().total += sizeof(event.xmotion.time) + sizeof(event.xmotion.x) + sizeof(&event.xmotion.y);
+        instance.output();
+        break;
+      }
+      case KeyRelease:
+      {
+        instance().hash.Update(reinterpret_cast<CryptoPP::byte *>(&event.xkey.time), sizeof(event.xkey.time) / sizeof(CryptoPP::byte));
+        instance().hash.Update(reinterpret_cast<CryptoPP::byte *>(&event.xkey.keycode), sizeof(event.xkey.keycode) / sizeof(CryptoPP::byte));
+        instance().hash.Update(reinterpret_cast<CryptoPP::byte *>(&event.xkey.state), sizeof(event.xkey.state) / sizeof(CryptoPP::byte));
+        instance().total += sizeof(event.xkey.time) + sizeof(event.xkey.keycode) + sizeof(&event.xkey.state);
+        instance.output();
         break;
       }
       default:
